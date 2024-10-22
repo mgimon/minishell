c1a78f24502366c46fcc42478101adccdec84881
Manuel Gimon Caballero <mgimon-c@cbr2s6.42barcelona.com>
2024-10-22 22:22:25 +0200

    22102024

--- my_minishell_current/utils_11.c
+++ my_minishell_current/utils_11.c
@@ -6,7 +6,7 @@
 /*   By: mgimon-c <mgimon-c@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 20:35:51 by mgimon-c          #+#    #+#             */
-/*   Updated: 2024/10/21 20:23:30 by mgimon-c         ###   ########.fr       */
+/*   Updated: 2024/10/22 22:14:10 by mgimon-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -153,10 +153,10 @@ void	remove_lastdir(t_section *current, char **var_pwd)
 	if (!(*var_pwd) || !(current->cmdv) || count_lines(current->cmdv) == 1)
 		return ;
 	if (ft_strcmp(current->cmdv[1], "..") != 0
-		|| ft_strcmp(current->cmdv[1], "/..") != 0
-		|| ft_strcmp(current->cmdv[1], "../") != 0)
+		&& ft_strcmp(current->cmdv[1], "/..") != 0
+		&& ft_strcmp(current->cmdv[1], "../") != 0)
 		return ;
-	if (ft_strcmp((*var_pwd), "PWD="))
+	if (ft_strcmp((*var_pwd), "PWD=") == 0)
 		return ;
 	while ((*var_pwd)[i])
 		i++;
@@ -178,6 +178,64 @@ void	remove_lastdir(t_section *current, char **var_pwd)
 	*var_pwd = new_varpwd;
 }
 
+void	change_last_subdir(t_section *current, char **var_pwd)
+{
+	int		i;
+	int		n;
+	int		k;
+	char	*result;
+	char	*tmp;
+
+	i = 0;
+	n = 0;
+	if (!current->cmdv || !var_pwd)
+		return ;
+	while ((*var_pwd)[i])
+		i++;
+	while ((*var_pwd)[i] != '/')
+	{
+		i--;
+		n++;
+	}
+	result = malloc(sizeof(char) * ((i - n) + 1));
+	k = 0;
+	while (k < (i - n + 1))
+	{
+		result[k] = (*var_pwd)[k];
+		k++;
+	}
+	k = '\0';
+	tmp = ft_strjoin(result, (current->cmdv[1] + 2));
+	free(result);
+	free(*var_pwd);
+	*var_pwd = ft_strdup(tmp);
+	free(tmp);
+}
+
+int	has_dots_and_subdir(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+		i++;
+	while (i > 0)
+	{
+		if (i >= 2 && str[i] == '/')
+		{
+			if (str[i - 1] == '.')
+			{
+				if (str[i - 2] == '.')
+					return (1);
+			}
+		}
+		i--;
+	}
+	return (0);
+}
+
 void	update_pwds(t_section *current, char **var_pwd, char **var_oldpwd)
 {
 	char	*clean_varpwd;
@@ -185,16 +243,12 @@ void	update_pwds(t_section *current, char **var_pwd, char **var_oldpwd)
 	char	*clean_home;
 	char	*tmp;
 
-	// dereference *vars
-	// get clean var_pwd and var_oldpwd;
 	clean_varpwd = ft_strdup((*var_pwd) + 4);
 	clean_varoldpwd = ft_strdup((*var_oldpwd) + 7);
 	(void)clean_varoldpwd;
 	(void)clean_varpwd;
-	// gestionar ../
 	if ((count_lines(current->cmdv) == 1) || (ft_strcmp(current->cmdv[1], "~") == 0))
 	{
-		put_str_fd(2, "wooo\n");
 		clean_home = get_home(current->info->env);
 		if ((count_lines(current->cmdv) == 1) || (ft_strcmp(current->cmdv[1], "~") == 0))
 			chdir(clean_home);
@@ -208,6 +262,15 @@ void	update_pwds(t_section *current, char **var_pwd, char **var_oldpwd)
 		*var_pwd = ft_strdup(tmp);
 		free(tmp);
 		free(clean_home);
+	}
+	else if (has_dots_and_subdir(current->cmdv[1]))
+	{
+		if (*var_oldpwd)
+		{
+			free(*var_oldpwd);
+			*var_oldpwd = ft_strjoin("OLD", *var_pwd);
+		}
+		change_last_subdir(current, var_pwd);
 	}
 	else if (has_slash(current->cmdv[1]))
 	{
