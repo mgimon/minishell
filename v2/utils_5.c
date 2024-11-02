afbef8c521cc692f9a005f37dc318c6f61a35172
mgimon-c <m.gimoncaballero@gmail.com>
2024-11-02 05:32:18 +0100

    v2 con subdivision de funciones

---
+++ v2/utils_5.c
@@ -0,0 +1,127 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_5.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgimon-c <mgimon-c@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/30 21:35:56 by mgimon-c          #+#    #+#             */
+/*   Updated: 2024/10/30 11:42:10 by mgimon-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	add_str_to_matrix(char ***matrix, char *str)
+{
+	int		i;
+	char	*duplicate;
+	char	**new_matrix;
+
+	if (!matrix || !str)
+		return ;
+	i = 0;
+	while ((*matrix) && (*matrix)[i])
+		i++;
+	new_matrix = (char **)malloc(sizeof(char *) * (i + 2));
+	if (!new_matrix)
+		return ;
+	i = 0;
+	while ((*matrix) && (*matrix)[i])
+	{
+		new_matrix[i] = ft_strdup((*matrix)[i]);
+		i++;
+	}
+	duplicate = ft_strdup(str);
+	new_matrix[i] = duplicate;
+	new_matrix[i + 1] = NULL;
+	if (*matrix)
+		matrix_free(*matrix);
+	*matrix = new_matrix;
+}
+
+void	allocate_heredocs(t_section *section, t_token *first)
+{
+	t_token		*tmp;
+	t_heredoc	*tmp_hdocs;
+	t_heredoc	*new_hdoc;
+	int			count;
+
+	tmp = first;
+	count = 0;
+	new_hdoc = NULL;
+	if (!first)
+		return ;
+	while (tmp)
+	{
+		if (ft_strcmp(tmp->str, "<<") == 0)
+			count++;
+		tmp = tmp->next;
+	}
+	if (!count)
+	{
+		section->heredocs = NULL;
+		return ;
+	}
+	section->heredocs = malloc(sizeof(t_heredoc));
+	tmp_hdocs = section->heredocs;
+	tmp = first;
+	allocate_heredocs_helper(tmp, tmp_hdocs, new_hdoc, count);
+}
+
+char	*clean_str_exit(char *str)
+{
+	int		i;
+	int		j;
+	char	*result;
+
+	i = 0;
+	j = 0;
+	while (str[i] == ' ')
+		i++;
+	if (!(str[i] >= '0' && str[i] <= '9'))
+		return (NULL);
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		i++;
+		j++;
+	}
+	while (str[i] == ' ')
+		i++;
+	if (str[i] != '\0')
+		return (NULL);
+	result = malloc(sizeof(char) * (j + 1));
+	clean_str_exit_helper(result, str);
+	return (result);
+}
+
+void	write_in_heredocs(t_section *current, t_general *info)
+{
+	char		buffer[1024];
+	t_heredoc	*tmp_hdoc;
+
+	tmp_hdoc = current->heredocs;
+	if (!current || !current->heredocs)
+		return ;
+	while (tmp_hdoc)
+	{
+		write_in_heredocs_helper(tmp_hdoc, buffer, info);
+		close(tmp_hdoc->fds[1]);
+		if (buffer[0] == '\0')
+			break ;
+		tmp_hdoc = tmp_hdoc->next;
+	}
+}
+
+void	close_section_hdocs_parent(t_section *current)
+{
+	t_heredoc	*tmp;
+
+	tmp = current->heredocs;
+	while (tmp)
+	{
+		close(tmp->fds[0]);
+		close(tmp->fds[1]);
+		tmp = tmp->next;
+	}
+}
